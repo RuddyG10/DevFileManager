5cda1b4c6110638c2e8c40f2e2e31e1405ace9ca
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-19 10:24:45 -0400

    created the run program

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Application.Parsing;
+using Console;
+using Infrastructure.FileSystem;
+
+var commandParser = new CommandParser();
+var fileSystemService = new FileSystemService();
+var pathResolver = new PathResolver();
+
+var application = new ConsoleApplication(commandParser, fileSystemService, pathResolver);
+application.Run();
